749ecb05d7c45b25edf5f0cb13489b5a12ce0268
ViperBite03 <layitamartin@gmail.com>
2025-05-08 12:48:22 +0200

    ...

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@ const nextPage = () => {
 
 <style lang="scss">
 #grid-container {
-  background-image: url('https://media.canva.com/v2/image-resize/format:JPG/height:1600/quality:92/uri:ifs%3A%2F%2F%2F661ebdb9-0b15-46a5-9305-2d612f523d9f/watermark:F/width:1066?csig=AAAAAAAAAAAAAAAAAAAAAFi2OgMUosYOu8aGx2PxNrzoWREttQScjXjecz5Nr3YO&exp=1746026325&osig=AAAAAAAAAAAAAAAAAAAAAKltDuAsvCXUrae2PJuyb69nsqbFenYJL1M4u-qP1Xth&signer=media-rpc&x-canva-quality=screen_2x');
+  background-image: url('src/assets/leaf-background.jpg');
   height: 100%;
 
   background-position: center;
--- src/components/paginaUno.vue
+++ src/components/paginaUno.vue
@@ -2,7 +2,7 @@
 
 <style scoped lang="scss">
 .content-container {
-  border: 1px solid violet;
+  //border: 1px solid violet;
   font-size: 65px;
   color: white;
   text-align: center;
@@ -20,9 +20,5 @@
   <div class="content-container">
     <h1>añsdf</h1>
     <span> hola </span>
-    <img
-      src="https://media.canva.com/v2/image-resize/format:PNG/height:800/quality:100/uri:ifs%3A%2F%2F%2F6f3bd683-4e6c-402f-ba6c-3b4edd4be1ac/watermark:F/width:800?csig=AAAAAAAAAAAAAAAAAAAAAPDUL3mrPVbzNQXQSBht4XGVesD2Fz3Jnqt5qdLS-W4W&exp=1746025048&osig=AAAAAAAAAAAAAAAAAAAAABil3c2JzGuAK9DFSIFxuInr9BY1Dap0nmthyJcIck0R&signer=media-rpc&x-canva-quality=screen"
-      alt=""
-    />
   </div>
 </template>
